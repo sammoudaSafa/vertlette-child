0d0cb4bdf56645d700ea9f2725a9f723ff051c49
SAFA <sammoudasaffa@gmail.com>
2020-09-15 19:47:14 -0400

    theme

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+   wp_enqueue_style( 'vertlette', get_template_directory_uri().'/style.css' );
 }
 
 
